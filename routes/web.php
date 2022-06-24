cd349442440c25855411d0de14c5ad255b4abd7a
laloxd666 <57124107+laloxd666@users.noreply.github.com>
2022-06-24 12:21:45 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/autores', [AutoresController::class, 'index']);
+Route::apiResource('apiAutor', AutoresController::class);
 
